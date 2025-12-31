2070479c4aebabda052cf5589ef4f5e2f939c073
Elle Carver <ellec@Elles-MacBook-Pro.local>
2025-12-30 22:54:03 -0500

    Configure for static export and build for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  basePath: '/quote-generator-app',
+  images: {
+    unoptimized: true
+  }
+}
 
 module.exports = nextConfig
-
